31f1e7b2c9babb24ca054d6d2117bf6f9cbfea8d
Michel <m.n.dore@students.uu.nl>
2019-11-04 02:08:37 +0100

    Ability to turn GUI on or off (comes in handy when training)

--- ai.py
+++ ai.py
@@ -5,21 +5,24 @@ class AI:
         gothere = ["N","S","E","W"]
         xh,yh = snake.body[0]
         xb,yb = snake.body[1]
-        if xh < xb:
-            gothere.remove("E")
-        elif xh > xb:
-            gothere.remove("W")
-        elif yh > yb:
-            gothere.remove("N")
-        else:
-            gothere.remove("S")
+        try:
+            if xh < xb:
+                gothere.remove("E")
+            elif xh > xb:
+                gothere.remove("W")
+            elif yh > yb:
+                gothere.remove("N")
+            else:
+                gothere.remove("S")
 
-        if xh+1 == 200:
-            gothere.remove("E")
-        elif xh-1 == 0:
-            gothere.remove("W")
-        elif yh-1 == 0:
-            gothere.remove("N")
-        elif yh + 1 == 200:
-            gothere.remove("S")
+            if xh+1 == 40:
+                gothere.remove("E")
+            elif xh-1 == 0:
+                gothere.remove("W")
+            elif yh-1 == 0:
+                gothere.remove("N")
+            elif yh + 1 == 40:
+                gothere.remove("S")
+        except:
+            print("Error")
         return random.choice(gothere)
--- game.py
+++ game.py
@@ -9,7 +9,7 @@ GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 size = (0, 0)
 screen = None
-scaling_factor = 4
+scaling_factor = 16
 
 class Game:
     def __init__(self, SCREEN_WIDTH, SCREEN_HEIGHT, amount_snakes=2):
@@ -19,7 +19,7 @@ class Game:
         self.snakes = [Snake(self.randomLocation()) for _ in range(amount_snakes)]
         self.food = self.randomFood()
 
-    def start(self):
+    def start(self,gui=True):
         def drawPixel(location, color):
             x,y=location
             pygame.draw.rect(screen, color, pygame.Rect(x*scaling_factor, y*scaling_factor, scaling_factor, scaling_factor))
@@ -32,40 +32,36 @@ class Game:
             for loc in s.body[1:]:
                 drawPixel(loc, WHITE)
 
-        pygame.init()                # if keys[pygame.K_LEFT]:
-                #     self.snakes[0].move("W",eating)
-                # if keys[pygame.K_RIGHT]:
-                #     self.snakes[0].move("E",eating)
-                # if keys[pygame.K_UP]:
-                #     self.snakes[0].move("N",eating)
-                # if keys[pygame.K_DOWN]:
-                #     self.snakes[0].move("S",eating)
-        size = (self.SCREEN_WIDTH, self.SCREEN_HEIGHT)
-        screen = pygame.display.set_mode(size)
-        pygame.display.set_mode((self.SCREEN_WIDTH*scaling_factor, self.SCREEN_HEIGHT*scaling_factor))
-        pygame.display.set_caption("Snake")
-        screen.fill(BLACK)
+        if gui:
+            pygame.init()
+            size = (self.SCREEN_WIDTH, self.SCREEN_HEIGHT)
+            screen = pygame.display.set_mode(size)
+            pygame.display.set_mode((self.SCREEN_WIDTH*scaling_factor, self.SCREEN_HEIGHT*scaling_factor))
+            pygame.display.set_caption("Snake")
+            screen.fill(BLACK)
+            clock = pygame.time.Clock()
 
         done = False
-        clock = pygame.time.Clock()
-        # -------- Main Program Loop -----------
         while not done:
-            screen.fill(BLACK)
             snakesToDelete = []
             for snake in self.snakes:
                 if not snake.death and self.snakeCollision(snake):
                     snakesToDelete.append(snake)
-            for snake in snakesToDelete:
-                snake.body  = []
-                snake.death = True
+                    for snake in snakesToDelete:
+                        snake.body  = []
+                        snake.death = True
 
-            [drawSnake(snake) for snake in self.snakes if not snake.death]
-            drawFood()
-            # --- Main event loop
-            for event in pygame.event.get():
+            if gui:
+                screen.fill(BLACK)
+                [drawSnake(snake) for snake in self.snakes if not snake.death]
+                drawFood()
                 if event.type == pygame.QUIT:
                     done = True
-            keys = pygame.key.get_pressed()
+                for event in pygame.event.get():
+                    keys = pygame.key.get_pressed()
+
+            if all([snake.death for snake in self.snakes]):
+                done = True
 
             eating = False
             for snake in self.snakes:
@@ -75,13 +71,12 @@ class Game:
 
             if eating: self.food = self.randomFood()
 
-            pygame.display.flip()
+            if gui:
+                pygame.display.flip()
+                clock.tick(30)
 
-            # --- Limit to 60 frames per second
-            clock.tick(30)
-
-        # Close the window and quit.
-        pygame.quit()
+        if gui:
+            pygame.quit()
 
     def randomLocation(self):
         (x,y) = (random.randint(0,self.SCREEN_WIDTH - 1),random.randint(0 + 4,self.SCREEN_HEIGHT))
@@ -114,4 +109,4 @@ class Game:
     def foodCollision(self, snake):
         return not snake.death and self.checkCollision(snake.body[0], self.food)
 
-Game(200,200).start()
+Game(40,40).start(gui=False)
